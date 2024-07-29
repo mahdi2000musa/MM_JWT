41a816d9fd3c8e04110d62d4228ebdedd5e0dd30
meti <mmehdi2022@gmail.com>
2024-07-29 13:21:38 +0330

    test

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.4' 
+VERSION = '0.0.5' 
 DESCRIPTION = 'Django rest framework token authentication'
 LONG_DESCRIPTION = 'An open source project created by a group of three just in case you want custom tokens'
 
